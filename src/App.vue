57df9a6b21445d5f876c7acbb31072756382bc34
zeyd <1zeydinik@gmail/com>
2023-10-14 17:52:21 +0200

    Revise NavBar component
    Added media queries and desktop look

--- src/App.vue
+++ src/App.vue
@@ -12,4 +12,6 @@ import Navbar from '@/components/Navbar.vue';
 
 <style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700&family=Poppins:ital,wght@0,700;1,400;1,800&display=swap');
+
+
 </style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,9 +2,22 @@
 /* icon library */
 import { Icon } from '@iconify/vue';
 import { RouterLink, RouterView } from 'vue-router';
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 
-const mobil = ref(true);
+onMounted(() => {
+    resize();
+    window.addEventListener('resize', resize);
+});
+
+function resize() {
+    if (window.innerWidth >= 1024) {
+        mobile.value = false;
+    } else {
+        mobile.value = true;
+    }
+}
+
+const mobile = ref(true);
 const isNavOpen = ref(false);
 </script>
 
@@ -12,13 +25,21 @@ const isNavOpen = ref(false);
     <nav>
         <div class="navbar">
             <div class="navbar-A">
-                <RouterLink :to="{ name: 'ViewNotes' }"><p class="app-name">ViiNotes</p></RouterLink>
-
-                <div class="nav-icons">
+                <RouterLink :to="{ name: 'ViewNotes' }">
+                    <p class="app-name">ViiNotes</p>
+                </RouterLink>
+                <div v-if="mobile" class="nav-icons">
                     <Icon icon="uis:bars" class="icon" v-if="!isNavOpen" @click="isNavOpen = !isNavOpen" />
                     <Icon icon="ph:x" class="icon" v-if="isNavOpen" @click="isNavOpen = !isNavOpen" />
                 </div>
+                <div v-else>
+                    <div class="desktop-links">
+                        <RouterLink :to="{ name: 'ViewNotes' }">Notes</RouterLink>
+                        <RouterLink :to="{ name: 'ViewStats' }">Stats</RouterLink>
+                    </div>
+                </div>
             </div>
+
             <Transition name="nav">
                 <div class="navbar-B" v-if="isNavOpen">
                     <RouterLink :to="{ name: 'ViewNotes' }">Notes</RouterLink>
@@ -33,15 +54,18 @@ const isNavOpen = ref(false);
 .navbar {
     font-family: 'Open Sans', sans-serif;
     font-family: 'Poppins', sans-serif;
+    background-color: #48c78e;
+    position: relative;
+    padding: 0.4rem;
 
     &-A {
         font-size: larger;
         padding: 0.5rem 1rem;
         letter-spacing: 0.8px;
-        background-color: #48c78e;
         color: white;
-
         letter-spacing: 0.8px;
+        max-width: 1024px;
+        margin: 0 auto;
 
         display: flex;
         justify-content: space-between;
@@ -60,18 +84,33 @@ const isNavOpen = ref(false);
                 }
             }
         }
+        .desktop-links a {
+            margin-left: 12px;
+            font-size: 17px;
+
+            height: 100%;
+            width: auto;
+            &.router-link-active {
+                padding-bottom: 2px;
+                border-bottom: 3px solid rgb(207, 236, 204);
+            }
+        }
     }
     &-B {
         position: fixed;
         right: 0;
         left: 0;
-        z-index: 3;
-        transform-origin: top center;
-
         display: flex;
         flex-direction: column;
 
         box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
+        transform-origin: top center;
+        background-color: white;
+        @media (min-width: 1024px) {
+            position: absolute;
+            right: 5px;
+            top: 5px;
+        }
 
         a {
             text-decoration: none;
